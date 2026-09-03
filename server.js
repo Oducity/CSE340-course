5eb14df3e740adb1c9ed5df48460dd2426798859
Oducity <henry.odumamwen@gmail.com>
2026-09-03 14:39:16 +0100

    Started the server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Hello, Node.js!");
+import express from "express";
+
+const NODE_ENV = "production";
+//const PORT = process.env.PORT || 3000;
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Node.js!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
