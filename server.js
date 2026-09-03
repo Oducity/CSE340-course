2d050d8d146c468460532538c28d6b95feafc2dc
Oducity <henry.odumamwen@gmail.com>
2026-09-03 16:44:34 +0100

    Imports fileURLRoPath and path for the configuration of express middleware to serve public file for easy access by the client side application.

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 import express from "express";
+import { fileURLToPath } from "url"; // import fileURLToPath from "url" to get the current file path
+import path from "path"; // import path from "path" to get the current directory path
+
+/*
+In the code bellow, import.meta.url gives the URL of the current module, 
+fileURLToPath() converts that URL to a file system path,
+path.dirname() extracts just the directory portion. This recreates
+the same functionality that CommonJS provided automatically.
+*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Define the environment and port
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "development";
@@ -7,6 +18,13 @@ const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "development";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+/*
+Configure the Express middleware (i.e app) to serve static files from the "public" directory.
+This means that any files in the "public" directory can be accessed directly via their URL.
+For example, if there's a file "public/css/main.css", it can be accessed at "http://localhost:3000/css/main.css".
+*/
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello, Node.js!");
