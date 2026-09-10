3a2f7ca836ea18f21bb609a89c720977ff7b41bf
Oducity <henry.odumamwen@gmail.com>
2026-09-10 11:29:25 +0100

    define the getAllOrganizations() function, query the database, return result.rows and export the function.

---
+++ src/models/organizations.js
@@ -0,0 +1,12 @@
+import db from "./db.js";
+
+
+const getAllOrganizations = async () => {
+    const sqlQuery = `SELECT organization_id, organization_name, organization_email, description, logo_filename
+                FROM organizations;
+                    `;
+    const result = await db.query(sqlQuery);
+  return result.rows;
+};
+
+export { getAllOrganizations };
