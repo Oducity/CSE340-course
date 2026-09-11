838271fcb5fa2c2bb8b68ce60c6d638e1fb12e17
Oducity <henry.odumamwen@gmail.com>
2026-09-11 16:01:24 +0100

    Displayed the all project details in the service projects page using the projects.ejs file and tweaked the css to make the service projects page and the footer/herder more visual appealing.

--- server.js
+++ server.js
@@ -51,8 +51,7 @@ app.get("/organizations", async (req, res) => {
 app.get("/projects", async (req, res) => {
   const projects = await getAllProjects(); // Fetch all projects from the database
   const title = "Service Projects";
-  console.log(projects); // Log the projects data to the console for debugging purposes
-  res.render("projects", { title }); // Render the "projects" view and pass the title and projects data to the template
+  res.render("projects", { title, projects }); // Render the "projects" view and pass the title and projects data to the template
 });
 
 app.get("/category", async (req, res) => {
