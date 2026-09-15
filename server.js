da9bda33f13085003d6dffea969d9c436d8c1c3e
Oducity <henry.odumamwen@gmail.com>
2026-09-15 05:01:39 +0200

    Developed  global middleware to display development mode within the footer.

--- server.js
+++ server.js
@@ -36,6 +36,17 @@ app.set("view engine", "ejs");
 // Set the directory for view templates to "src/views".
 app.set("views", path.join(__dirname, "src/views"));
 
+// Set a middleware that log the development mode
+app.use((req, res, next) => {
+  if (NODE_ENV === "development") {
+    console.log(`${req.method} ${req.url}`);
+  }
+  next();
+});
+app.use((req, res, next) => {
+  res.locals.NODE_ENV = NODE_ENV;
+  next();
+});
 // Route handlers for different paths.
 // Each route sends a specific HTML file as a response.
 app.get("/", async (req, res) => {
