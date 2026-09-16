ea56f377203014d6e0dacc91ff8a12c13b85202b
Oducity <henry.odumamwen@gmail.com>
2026-09-16 19:34:35 +0100

    Added comment statements for record purposes.

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -27,8 +27,18 @@ const getOrganizationDetails = async (organizationId) => {
     WHERE organization_id = $1;
   `;
 
-  const queryParams = [organizationId];
-  const result = await db.query(sqlQuery, queryParams);
+  /**
+   * IMPORTANT SECURITY NOTE
+   * Notice that this function uses a parameterized query to safely include the organization ID in the SQL statement,
+   * which helps prevent SQL injection attacks. If you included the organization ID directly in the query string,
+   * it could allow malicious users to execute arbitrary SQL commands.
+   * In this course, you should always use parameterized queries when including user input in SQL statements.
+   * Never put the user input directly into the query string.
+   */
+
+  const queryParams = [organizationId]; // The user input value contained in organizationId is stored in a variable called queryParams.
+  const result = await db.query(sqlQuery, queryParams); // The queryParams is passed to the database to be safely substituted for
+  // the organization_id in the WHERE clause by the database itself for safe and secure query/filter
 
   // Return the first row of the result set or null if no rows are found.
   return result.rows.length > 0 ? result.rows[0] : null;
