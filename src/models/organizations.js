8701d024a46739fc18a34b404b28eda720eb61ff
Oducity <henry.odumamwen@gmail.com>
2026-09-16 19:21:01 +0100

    Developed the getOrganizationDetails function to safely query the database for organization details.

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,11 +1,36 @@
 import db from "./db.js";
 
+// Function to query organization table
 const getAllOrganizations = async () => {
-  const sqlQuery = `SELECT organization_id, organization_name, organization_email, description, logo_filename
-        FROM organizations;
+  const sqlQuery = `
+    SELECT
+      organization_id,
+      organization_name,
+      organization_email,
+      description,
+      logo_filename
+    FROM organizations;
                     `;
   const result = await db.query(sqlQuery);
   return result.rows;
 };
 
-export { getAllOrganizations };
+const getOrganizationDetails = async (organizationId) => {
+  const sqlQuery = `
+    SELECT
+      organization_id,
+      organization_name,
+      organization_email,
+      description,
+      logo_filename
+    FROM organizations
+    WHERE organization_id = $1;
+  `;
+
+  const queryParams = [organizationId];
+  const result = await db.query(sqlQuery, queryParams);
+
+  // Return the first row of the result set or null if no rows are found.
+  return result.rows.length > 0 ? result.rows[0] : null;
+};
+export { getAllOrganizations, getOrganizationDetails };
