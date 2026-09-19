a805af83072aa2e2ac1e268e68b8e9be5ad272af
Oducity <henry.odumamwen@gmail.com>
2026-09-19 17:24:41 +0100

    Developed getCategoryRelatedProjects model function to get all projects related to a particular category.

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,5 +1,6 @@
 import db from "./db.js";
 
+// This function get all categories data from the database.
 const getAllCategories = async () => {
   const sqlQuery = `SELECT category_name, category_description, created_at
                 FROM category;`;
@@ -7,4 +8,24 @@ const getAllCategories = async () => {
   return rows;
 };
 
-export { getAllCategories };
+// Using the id object, this function get a single category data from the database
+const getCategoryRelatedProjects = async (categoryId) => {
+  const sqlQuery = `
+    SELECT
+      category_name,
+      projects.project_id,
+      projects.project_name
+    FROM category
+      INNER JOIN projects_category 
+        ON projects_category.category_id = category.category_id
+      INNER JOIN projects
+        ON projects.project_id = projects_category.project_id
+    WHERE category_id = $1
+    ORDER BY projects.project_date ASC;
+  `;
+
+  const queryParams = [categoryId]; // Get the category id from the input.
+  const result = db.query(sqlQuery, queryParams); // Query the database allowing the database to compare for the categoryId on its own for safety.
+  return result.rows > 0 ? result.rows[0] : null; // Check and return the result row if the rows found is greater than 0.
+};
+export { getAllCategories, getCategoryRelatedProjects };
