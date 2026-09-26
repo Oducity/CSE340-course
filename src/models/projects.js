4d35c21adb9e9cd05b848227c7ae50f78ed84bd1
Oducity <henry.odumamwen@gmail.com>
2026-09-26 02:15:20 +0100

    Removed createProject function from the projects.js in the model.

--- src/models/projects.js
+++ src/models/projects.js
@@ -144,7 +144,6 @@ const getAllCategoryTagsByProjectId = async (projectId) => {
       : [];
 };
 
-
 // Export getAllProjects and getProjectsByOrganizationId functions for use in other parts of the application, such as in server.js file.
 export {
   getAllProjects,
