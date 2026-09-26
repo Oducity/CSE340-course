ac83bf2ef58339db81d4d690bf559b27a222eef9
Oducity <henry.odumamwen@gmail.com>
2026-09-26 22:18:43 +0100

    Created the updateCate.goryAssignments and assignCategoryToProject model functions in the projects.js

--- src/models/categories.js
+++ src/models/categories.js
@@ -28,4 +28,34 @@ const getCategoryDetailsById = async (categoryId) => {
   const result = await db.query(sqlQuery, idOfCategory);
   return result.rows.length > 0 ? result.rows[0] : null;
 };
-export { getAllCategories, getCategoryDetailsById };
+
+// This function assigns categories to a project
+const assignCategoryToProject = async (projectId, categoryId) => {
+  const sqlQuery = `
+    INSERT INTO projects_category (project_id, category_id)
+    VALUES ( $1, $2 )
+    RETURNING project_id, category_id;
+  `;
+
+  await db.query(sqlQuery, [projectId, categoryId]);
+};
+
+// This function update categories to a project
+const updateCategoryAssignments = async (projectId, categoryIds) => {
+  const sqlQuery = `
+      DELETE FROM projects_category
+      WHERE project_id = $1;
+    `;
+
+  await db.query(sqlQuery, [projectId]);
+
+  for (const categoryId of categoryIds) {
+    await assignCategoryToProject(projectId, categoryId);
+  }
+};
+export {
+  getAllCategories,
+  getCategoryDetailsById,
+  assignCategoryToProject,
+  updateCategoryAssignments,
+};
